8f4cfa78baa4b55561912b6721a193213807242e
SashaP-star <sashafrik17@gmail.com>
2025-03-10 15:28:50 +0000

    Add files via upload

---
+++ c1/c1/c1/Source2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main2()
+{
+	int a = 1;
+	int b;
+	std::cout << a;
+	std::cout << "Input value a: ";
+
+	std::cin >> a;
+	std::cout << "\nInput value b:";
+	std::cin >> b;
+	std::cout << "you input " << a * b;
+
+
+	return 0;
+}
